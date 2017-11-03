8702da199af2082cc4a74fb53c6f27a9c5fbb4ea
Alex Daum <daumalex10200@gmail.com>
2017-11-03 22:50:36 +0100

    1.10.2 Release 1.1

---
+++ src/main/java/at/xander/jrftl/JEIConfig.java
@@ -0,0 +1,41 @@
+package at.xander.jrftl;
+
+import mezz.jei.api.IJeiRuntime;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.ISubtypeRegistry;
+import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.ingredients.IIngredientBlacklist;
+import mezz.jei.api.ingredients.IModIngredientRegistration;
+import net.minecraft.item.ItemStack;
+
+@JEIPlugin
+public class JEIConfig implements IModPlugin {
+
+	@Override
+	public void register(IModRegistry registry) {
+		IIngredientBlacklist blacklist = registry.getJeiHelpers().getIngredientBlacklist();
+		if (!JRFTL.instance.isHardMode()) {
+			blacklist.addIngredientToBlacklist(new ItemStack(JRFTL.instance.PreparedFlesh));
+		}
+	}
+
+	@Override
+	public void registerItemSubtypes(ISubtypeRegistry subtypeRegistry) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void registerIngredients(IModIngredientRegistration registry) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
